6415b31434a7502f40285a8f1050169a6e74b272
Adam Sasine <adam.sasine@gmail.com>
2024-03-05 08:53:46 -0500

    Added `Signed-By` option to APT sources
    Changed APT sources to use DEB822 format
    
    Closes #1

--- add-apt-source.bash
+++ add-apt-source.bash
@@ -3,4 +3,12 @@ source env.bash
 cp repo/KEY.asc /etc/apt/keyrings/$REPO_NAME.asc
 
 # TODO: detect arch
-echo "deb [arch=all] https://$REPO_NAME.blob.core.windows.net/debian $(lsb_release -cs) unstable" > "/etc/apt/sources.list.d/$REPO_NAME.list"
+cat <<EOF > "/etc/apt/sources.list.d/$REPO_NAME.sources"
+Types: deb
+URIs: https://$REPO_NAME.blob.core.windows.net/debian
+Suites: $SUITE
+Components: $COMPONENT
+Signed-By: /etc/apt/keyrings/$REPO_NAME.asc
+Architectures: all
+
+EOF
