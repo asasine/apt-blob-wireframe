a1e9e5462c1a7c135a0c6a7d3f85c0f5456a090e
Adam Sasine <adam.sasine@gmail.com>
2024-03-05 10:02:56 -0500

    Added Packages merging

--- update-repo.bash
+++ update-repo.bash
@@ -1,13 +1,26 @@
 #!/bin/bash
 source env.bash
-mkdir -p repo && pushd repo
+mkdir -p repo && pushd repo > /dev/null
 mkdir -p dists/$SUITE
 arch=all # TODO: detect arch
 mkdir -p dists/$SUITE/$COMPONENT/binary-$arch
 mkdir -p pool/$COMPONENT/binary-$arch
 
 gpg --armor --yes --output KEY.asc --export $GPG_USER_ID
-apt-ftparchive packages pool/$COMPONENT/binary-$arch > dists/$SUITE/$COMPONENT/binary-$arch/Packages
+
+# Update the local Packages file with apt-get and, if it exists, merge it with the existing Packages file.
+# This may fail if the local Packages file is not present, such as before the first upload.
+packages_file="/var/lib/apt/lists/${REPO_NAME}.blob.core.windows.net_debian_dists_${SUITE}_${COMPONENT}_binary-${arch}_Packages"
+sudo apt-get update -o Dir::Etc::sourcelist="/etc/apt/sources.list.d/$REPO_NAME.sources" -o Dir::Etc::sourceparts='-' -o APT::Get::List-Cleanup='0' > /dev/null 2>&1
+if [[ $? == 0 && -f $packages_file ]]; then
+    echo 'Merging with existing Packages'
+    cat "$packages_file" > dists/$SUITE/$COMPONENT/binary-$arch/Packages
+    apt-ftparchive packages pool/$COMPONENT/binary-$arch >> dists/$SUITE/$COMPONENT/binary-$arch/Packages
+else
+    echo 'Creating Packages'
+    apt-ftparchive packages pool/$COMPONENT/binary-$arch > dists/$SUITE/$COMPONENT/binary-$arch/Packages
+fi
+
 gzip -k -f dists/$SUITE/$COMPONENT/binary-$arch/Packages
 apt-ftparchive release dists/$SUITE \
     -o APT::FTPArchive::Release::Suite="$SUITE" \
